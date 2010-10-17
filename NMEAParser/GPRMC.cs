99fd607fc4e538c96399ad85a233117aace2d49c
david@wimsey.us <david@wimsey.us@2a446dd4-fd74-985f-c1a8-7930cfe0c0f0>
2010-10-17 05:49:40 +0000

    Initial import of NMEA sentence parser with generic serial device support.

---
+++ NMEAParser/GPRMC.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NMEAParser
+{
+	public class GPRMC
+	{
+		public readonly double Latitude;
+		public readonly double Longitude;
+		public readonly double Course;
+		public readonly double Speed;
+		public readonly bool GPSFix;
+		public readonly double MagneticVariation;
+		public readonly DateTime FixTimeStamp;
+
+		internal GPRMC(double lat, double lon, double course, double speed, bool isGPSFix, double MagVariation, DateTime UTCTimeStamp)
+		{
+			this.Latitude = lat;
+			this.Longitude = lon;
+			this.Course = course;
+			this.Speed = speed;
+			this.GPSFix = isGPSFix;
+			this.MagneticVariation = MagVariation;
+			this.FixTimeStamp = UTCTimeStamp;
+		}
+
+		internal static GPRMC ParseFields(string[] Fields)
+		{
+			bool f;
+			double lat = 0.0;
+			double lon = 0.0;
+			double s;
+			double b;
+			double m;
+			DateTime time;
+			if(String.IsNullOrEmpty(Fields[9])) {
+				throw new Exception("Missing fix date field for NMEA sentence: " + Fields[0]);
+			}
+			if(!String.IsNullOrEmpty(Fields[1])) {
+				time = NMEAParser.ParseTime(NMEAParser.ParseDate(Fields[9]), Fields[1]);
+			} else {
+				throw new Exception("Missing fix timestamp field for NMEA sentence: " + Fields[0]);
+			}
+			
+			if(String.IsNullOrEmpty(Fields[2])) {
+				throw new Exception("Missing GPS Fix valid flag for NMEA sentence: " + Fields[0]);
+			}
+			if("A".Equals(Fields[2])) {
+				f = true;
+			} else {
+				f = false;
+			}
+			if(String.IsNullOrEmpty(Fields[3]) || String.IsNullOrEmpty(Fields[4]) || String.IsNullOrEmpty(Fields[5]) || String.IsNullOrEmpty(Fields[6])) {
+				throw new Exception("Missing latitude and/or longitude field for NMEA sentence: " + Fields[0]);
+			}
+			lat = NMEAParser.ParseLatLon(Fields[3], Fields[4]);
+			lon = NMEAParser.ParseLatLon(Fields[5], Fields[6]);
+
+			if(!String.IsNullOrEmpty(Fields[7])) {
+				s = Double.Parse(Fields[7]);
+			} else {
+				throw new Exception("Missing speed field for NMEA sentence: " + Fields[0]);
+			}
+			if(!String.IsNullOrEmpty(Fields[8])) {
+				b = Double.Parse(Fields[8]);
+			} else {
+				throw new Exception("Missing course field for NMEA sentence: " + Fields[0]);
+			}
+			if(!String.IsNullOrEmpty(Fields[10])) {
+				m = Double.Parse(Fields[10]);
+			} else {
+				throw new Exception("Missing magnetic variation field for NMEA sentence: " + Fields[0]);
+			}
+			return (new GPRMC(lat, lon, b, s, f, m, time));
+		}
+	}
+}
