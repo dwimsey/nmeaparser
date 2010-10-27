f1672dd82ac825a420f72281d6a47b8f01f544dc
david@wimsey.us <david@wimsey.us@2a446dd4-fd74-985f-c1a8-7930cfe0c0f0>
2010-10-27 18:44:04 +0000

    Stub classes to prevent parser errors with the current demo logfiles we're reverse engineering

---
+++ NMEAParser/SentenceHandlers/GPGSA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NMEAParser.SentenceHandlers
+{
+	public class GPGSA
+	{
+		internal GPGSA()
+		{
+		}
+	}
+
+	public class GPGSAHandler : BaseSentenceHandler
+	{
+		internal GPGSAHandler()
+		{
+			p_Name = "GPGSA";
+		}
+
+		/// <summary>
+		/// Parses GPGSA sentences into GPGSA objects.
+		/// </summary>
+		/// <remarks>
+		/// Sentence Format: <code>$GPGSA</code>
+		/// Sentence Example: <code>$GPGSA</code>
+		/// </remarks>
+		/// <param name="Fields">Sentence fields for the sentence to be parsed.</param>
+		/// <returns>A GPGSA object representing the data in Fields</returns>
+		public override object ParseSentence(string[] Fields)
+		{
+			return (new GPGSA());
+		}
+	}
+}
---
+++ NMEAParser/SentenceHandlers/GPGSV.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NMEAParser.SentenceHandlers
+{
+	public class GPGSV
+	{
+		internal GPGSV()
+		{
+		}
+	}
+
+	public class GPGSVHandler : BaseSentenceHandler
+	{
+		internal GPGSVHandler()
+		{
+			p_Name = "GPGSV";
+		}
+
+		/// <summary>
+		/// Parses GPGSV sentences into GPGSV objects.
+		/// </summary>
+		/// <remarks>
+		/// Sentence Format: <code>$GPGSV</code>
+		/// Sentence Example: <code>$GPGSV</code>
+		/// </remarks>
+		/// <param name="Fields">Sentence fields for the sentence to be parsed.</param>
+		/// <returns>A GPGSV object representing the data in Fields</returns>
+		public override object ParseSentence(string[] Fields)
+		{
+			return (new GPGSV());
+		}
+	}
+}
